a964de9b4794e608d45745bd5a6a9c9997ca8053
18763761709 <2112529@mail.nankai.edu.cn>
2023-11-27 09:26:36 +0800

    Glider-R Init

---
+++ Glider-R/LSTM.py
@@ -0,0 +1,61 @@
+import tensorflow as tf
+from tensorflow.keras import layers
+
+import numpy as np
+
+def preprocess_data(memory_access_trace, sequence_length):
+    # Split the trace into fixed-length overlapping sequences
+    sequences = []
+    for i in range(0, len(memory_access_trace) - sequence_length, sequence_length // 2):
+        sequences.append(memory_access_trace[i:i + sequence_length])
+    return np.array(sequences)
+
+# Example usage
+sequence_length = 30  # As mentioned in the paper
+memory_access_trace = [...]  # Your memory access trace data
+processed_data = preprocess_data(memory_access_trace, sequence_length)
+
+
+
+# Define the LSTM model with attention
+class CacheReplacementModel(tf.keras.Model):
+    def __init__(self, pc_vocab_size, embedding_dim, lstm_units):
+        super(CacheReplacementModel, self).__init__()
+        self.embedding = layers.Embedding(input_dim=pc_vocab_size, output_dim=embedding_dim)
+        self.lstm = layers.LSTM(lstm_units, return_sequences=True)
+        self.attention = layers.Attention(use_scale=True)
+        self.output_layer = layers.Dense(1, activation='sigmoid')
+
+    def call(self, inputs):
+        x = self.embedding(inputs)
+        lstm_out = self.lstm(x)
+        attention_out = self.attention([lstm_out, lstm_out])
+        return self.output_layer(attention_out)
+
+# Example usage
+pc_vocab_size = 1000  # Replace with the actual size
+embedding_dim = 128
+lstm_units = 256
+
+model = CacheReplacementModel(pc_vocab_size, embedding_dim, lstm_units)
+
+# Compile the model
+# Compile the model
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+# Assuming you have processed_data and labels
+train_data = processed_data
+train_labels = [...]  # Binary labels for cache-friendly or cache-averse
+
+# Train the model
+epochs = 10  # Number of epochs
+batch_size = 32  # Batch size
+model.fit(train_data, train_labels, epochs=epochs, batch_size=batch_size)
+
+# Assuming you have test_data and test_labels
+test_data = [...]  # Test dataset
+test_labels = [...]  # Test labels
+
+# Evaluate the model
+model.evaluate(test_data, test_labels)
+
